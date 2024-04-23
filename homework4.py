7f661fdd8071e76f0e643e0f230234ecd25ad802
lithiumluba <lithiumluba@gmail.com>
2024-04-24 00:24:23 +0300

    added homework 4

---
+++ homework4.py
@@ -0,0 +1,8 @@
+# Request two integer numbers from user.
+# Output on one line expression with sum of numbers,
+# and on another line - expression with product of numbers:
+
+first = int(input("Enter a integer number, please: "))
+second = int(input("Enter one more integer number, please: "))
+print(first, '+', second, '=', first+second)
+print(first, '*', second, '=', first * second)
