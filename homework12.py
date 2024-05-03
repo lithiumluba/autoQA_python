876885d858de8b798bcde69ab88932be01a45c4b
lithiumluba <lithiumluba@gmail.com>
2024-05-04 02:36:36 +0300

    updated homework 12

--- homework12.py
+++ homework12.py
@@ -2,7 +2,9 @@
 # Print the all words, containing the letter 'o' (in any case (upper/lower)) twice, as a title.
 string_a = "This tool is cool. But that owl is awful. MAGIC TOOLS Ltd."
 word_list = string_a.title().split()
+print(word_list)
 result = ""
 for word in word_list:
     if word.count("o") == 2:
-        print(word)
+        result += word + " "
+print("Result:", result)
