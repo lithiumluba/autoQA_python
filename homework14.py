21b484aa0c35c3135375153eb9760fd5c72eb715
lithiumluba <lithiumluba@gmail.com>
2024-05-23 03:39:12 +0300

    added homework 14

---
+++ homework14.py
@@ -0,0 +1,27 @@
+# There is a list consisting of integer numbers.
+# Create three lists:
+# - to the first list add numbers that are only divisible by 3, but not divisible by 5
+# - to the second list add numbers that are divisible by 5, but not divisible by 3
+# - to the third list add numbers that are divisible by both 3 and 5
+
+input_lst = []
+div3 = []
+div5 = []
+div3and5 = []
+
+for i in range(16):
+    input_lst.append(int(input("Enter a number,please: ")))
+
+for item in input_lst:
+    if item % 3 == 0 and item % 5 == 0:
+        div3and5.append(item)
+        continue
+    if item % 3 == 0:
+        div3.append(item)
+        continue
+    if item % 5 == 0:
+        div5.append(item)
+        continue
+print(div3and5)
+print(div3)
+print(div5)
