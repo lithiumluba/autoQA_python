4bc156a549267bad714e78f0645e6e3558e20091
lithiumluba <lithiumluba@gmail.com>
2024-05-23 04:01:13 +0300

    added homework14

--- homework14.py
+++ homework14.py
@@ -12,6 +12,8 @@ div3and5 = []
 for i in range(16):
     input_lst.append(int(input("Enter a number,please: ")))
 
+print(input_lst, '# Input list')
+
 for item in input_lst:
     if item % 3 == 0 and item % 5 == 0:
         div3and5.append(item)
@@ -22,6 +24,7 @@ for item in input_lst:
     if item % 5 == 0:
         div5.append(item)
         continue
-print(div3and5)
-print(div3)
-print(div5)
+
+print(div3, ' # elements divided by 3')
+print(div5, ' # elements divided by 5')
+print(div3and5, ' # elements divided by 3 and by 5')
