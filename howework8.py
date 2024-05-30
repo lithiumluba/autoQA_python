a396aaa8072b3cbc4e15cdad5e26d102cb2be799
lithiumluba <lithiumluba@gmail.com>
2024-05-30 03:51:23 +0300

    updated homework 8

--- howework8.py
+++ howework8.py
@@ -9,31 +9,43 @@
 
 
 while True:
-    min_width = int(input("Enter minimal width: "))
-    max_width = int(input("Enter maximum width: "))
+    user_input1 = input("Enter q if you want to stop.\nEnter minimal width: ")
+    if user_input1 == 'q':
+        print("Thank you for attending! Bye!")
+        break
+    else:
+        try:
+            min_width = int(user_input1)
+            max_width = int(input("Enter maximum width: "))
 
-    if min_width > max_width:
-        print("Warning! The entered minimum width cannot be greater than the maximum width.")
-        exit()
-    if ((max_width - min_width) % 2) != 0:
-        print("Warning! The difference between the maximum and minimum widths cannot be multiple of 2")
-        exit()
+            if min_width > max_width:
+                print("Warning! The entered minimum width cannot be greater than the maximum width.")
+                exit()
+            if ((max_width - min_width) % 2) != 0:
+                print("Warning! The difference between the maximum and minimum widths cannot be multiple of 2")
+                exit()
 
-    for i in range(min_width, max_width + 1, 2):
-        spaces = ' ' * ((max_width - i) // 2)
-        if i == min_width:
-            stars = '*' * i
-            print(spaces + stars + spaces)
-            continue
-        stars = '*' + (i - 2) * " " + '*'
-        print(spaces + stars + spaces)
+            for i in range(min_width, max_width + 1, 2):
+                spaces = ' ' * ((max_width - i) // 2)
+                if i == min_width:
+                    stars = '*' * i
+                    print(spaces + stars)
+                    continue
+                stars = '*' + (i - 2) * " " + '*'
+                print(spaces + stars)
 
-    for i in range(max_width - 2, min_width - 1, -2):
-        spaces = ' ' * ((max_width - i) // 2)
-        if i == min_width:
-            stars = '*' * i
-            print(spaces + stars + spaces)
-            continue
-        stars = '*' + (i - 2) * " " + '*'
-        print(spaces + stars + spaces)
+            for i in range(max_width - 2, min_width - 1, -2):
+                spaces = ' ' * ((max_width - i) // 2)
+                if i == min_width:
+                    stars = '*' * i
+                    print(spaces + stars)
+                    continue
+                stars = '*' + (i - 2) * " " + '*'
+                print(spaces + stars)
+        except ValueError:
+            print("Entered value can’t be casted in 'int'")
 
+        else:
+            print('There is your diamond')
+        finally:
+            print("---------Another round---------")
